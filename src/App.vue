ac71c27891be1f381f5394ee529f90c0ee56a053
callmecposu <pc.cposu@gmail.com>
2023-07-05 18:44:21 -0400

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Reaction Timer</h1>
-  <button @click="start">Play</button>
-  <ReactionBlock />
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <ReactionBlock v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResults">Reaction Time: {{ result }} ms</p>
 </template>
 
 <script>
@@ -14,13 +15,20 @@ export default {
     return {
       isPlaying: false,
       delay: null,
+      result: null,
+      showResults: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000; // delay in MS
       this.isPlaying = true;
-      console.log(this.delay);
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.result = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     },
   },
 };
